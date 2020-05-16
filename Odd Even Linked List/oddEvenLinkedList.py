73fa148ad980fc9878cd11daf5ccd309c46601c3
varun21290 <varun21290@gmail.com>
2020-05-17 00:29:40 +0530

    Update oddEvenLinkedList.py

--- Odd Even Linked List/oddEvenLinkedList.py
+++ Odd Even Linked List/oddEvenLinkedList.py
@@ -5,7 +5,7 @@
 #         self.next = next
 class Solution:
     def oddEvenList(self, head: ListNode) -> ListNode:
-        if (head is None or head.next is None): return head
+        if (head is None): return head
         node=head
         temp1=node.next
         temp2=node.next
@@ -23,4 +23,20 @@ class Solution:
         node.next=temp1
         temp2.next=None
         return head
+    
+    
+    def oddEvenListBetter(self, head: ListNode) -> ListNode:
+        if (head is None): return head
+        odd=head
+        evenHead=head.next
+        even=evenHead
+        while( even is not None and even.next is not None):
+            odd.next=even.next
+            odd=odd.next
+            even.next=odd.next
+            even=even.next
+        odd.next=evenHead
+        return head
+    
+
         
