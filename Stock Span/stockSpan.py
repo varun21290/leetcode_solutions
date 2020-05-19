72eea2ca1bd818928c645fc686d459c1efc18d9b
varun21290 <varun21290@gmail.com>
2020-05-19 20:30:23 +0530

    Create stockSpan.py

---
+++ Stock Span/stockSpan.py
@@ -0,0 +1,23 @@
+class StockSpanner:
+    price_history=[]
+    price_map=[]
+    stack=[]
+    def __init__(self):
+        self.price_history=[]
+        self.price_map=[]
+        self.span=[]
+        
+    def next3(self, price: int) -> int:
+        j=len(self.price_history)-1
+        while(j!=-1 and self.price_history[j]<=price):
+            j=self.price_map[j]
+        self.price_history.append(price)
+        self.price_map.append(j)
+        return len(self.price_history)-j-1
+    
+    def next4(self, price: int) -> int:
+        span = 1
+        while self.stack and self.stack[-1][0] <= price:
+            span += self.stack.pop()[1]
+        self.stack.append((price, span)) 
+        return span
