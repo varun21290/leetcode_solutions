cde532e34530a0ab06e02f8f0edb808a769265a3
varun21290 <varun21290@gmail.com>
2020-05-19 20:31:44 +0530

    Update stockSpan.py

--- Stock Span/stockSpan.py
+++ Stock Span/stockSpan.py
@@ -7,7 +7,7 @@ class StockSpanner:
         self.price_map=[]
         self.span=[]
         
-    def next3(self, price: int) -> int:
+    def next(self, price: int) -> int:
         j=len(self.price_history)-1
         while(j!=-1 and self.price_history[j]<=price):
             j=self.price_map[j]
@@ -15,7 +15,7 @@ class StockSpanner:
         self.price_map.append(j)
         return len(self.price_history)-j-1
     
-    def next4(self, price: int) -> int:
+    def nextBetter(self, price: int) -> int:
         span = 1
         while self.stack and self.stack[-1][0] <= price:
             span += self.stack.pop()[1]
